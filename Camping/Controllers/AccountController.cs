2a4b219071d7fab69ee7b1c351e77b6e8bfa37e3
Joseph <pumphrey.joseph@gmail.com>
2019-02-20 11:13:09 -0800

    Register View model and more is done

---
+++ Camping/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Camping.Models;
+using Camping.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Camping.Controllers
+{
+    public class AccountController
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        /// Default Register Page
+        /// </summary>
+        /// <returns>Return the view of the register page</returns>
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel)
+    }
+}
