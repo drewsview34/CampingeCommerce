826a51f223694bcdf30a05e9454e1afc89107d83
shalina2 <shalom.belaineh@gmail.com>
2019-02-21 22:56:27 -0800

    added claim policy for age less than 18 not to purchase with credit card

---
+++ Camping/Models/Handler/MinimumAge.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Camping.Models.Handler
+{
+    public class MinimumAge : AuthorizationHandler<MinAgeRequirement>, IAuthorizationRequirment
+    {
+        private int _minimumAge;
+        public MinimumAge(int minimumAge)
+        {
+            _minimumAge = minimumAge;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinAgeRequirement requirement)
+        {
+           if(!context.User.HasClaim(c => c.Type == ClaimTypes.DateOfBirth))
+            {
+                return Task.CompletedTask;
+            }
+            DateTime dateOfBirth = Convert.ToDateTime(context.User.FindFirst(u => u.Type == ClaimTypes.DateOfBirth).Value);
+            int age = DateTime.Today.Year - dateOfBirth.Year;
+            if(dateOfBirth > DateTime.Today.AddYears(-age))
+            {
+                age--;
+            }
+            if(age >= _minimumAge)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
