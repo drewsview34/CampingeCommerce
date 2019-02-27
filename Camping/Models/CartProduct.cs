d9a979e000a650b7f6d2f91b2a76d018b3d64baa
Joseph <pumphrey.joseph@gmail.com>
2019-02-27 11:11:43 -0800

    Removed userID from CartProduct

--- Camping/Models/CartProduct.cs
+++ Camping/Models/CartProduct.cs
@@ -7,7 +7,6 @@ namespace Camping.Models
     {
         public int CartProductId { get; set; }
         public int CartID { get; set; }
-        public int UserID { get; set; }
         public int Quantity { get; set; }
         public int ProductID { get; set; }
         //I dont know if we want to rename our class inventory rather than product because that makes a whole lot sense.
